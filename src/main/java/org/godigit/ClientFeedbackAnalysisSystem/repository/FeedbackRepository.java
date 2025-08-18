a176743b013d0948742ddc3e792f820be5a52b52
Panav19 <panavkumar.sahu@gmail.com>
2025-08-18 09:55:09 +0530

    added annotation in repository

--- src/main/java/org/godigit/ClientFeedbackAnalysisSystem/repository/FeedbackRepository.java
+++ src/main/java/org/godigit/ClientFeedbackAnalysisSystem/repository/FeedbackRepository.java
@@ -2,7 +2,8 @@ package org.godigit.ClientFeedbackAnalysisSystem.repository;
 
 import org.godigit.ClientFeedbackAnalysisSystem.models.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 }
