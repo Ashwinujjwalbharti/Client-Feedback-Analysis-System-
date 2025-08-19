815f44a2b89a31a4c741ef0420890805c6609170
Panav19 <panavkumar.sahu@gmail.com>
2025-08-19 15:46:05 +0530

    Adding Filters and some changes in feedback controller

---
+++ src/main/java/org/godigit/ClientFeedbackAnalysisSystem/controller/FilterController.java
@@ -0,0 +1,32 @@
+package org.godigit.ClientFeedbackAnalysisSystem.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.godigit.ClientFeedbackAnalysisSystem.dto.FeedbackDto;
+import org.godigit.ClientFeedbackAnalysisSystem.mapper.FeedbackMapper;
+import org.godigit.ClientFeedbackAnalysisSystem.models.Feedback;
+import org.godigit.ClientFeedbackAnalysisSystem.service.FilterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/feedback/filter")
+public class FilterController {
+    
+    @Autowired
+    private FilterService filterService;
+
+    @GetMapping("/date")
+    public ResponseEntity<List<FeedbackDto>> filterFeedbackByDate(@RequestParam String dateString) {
+        LocalDate date = LocalDate.parse(dateString);
+        List<Feedback> feedbacks = filterService.filterByDate(date);
+        List<FeedbackDto> feedbackDtos = feedbacks.stream().map(FeedbackMapper :: toDto).collect(Collectors.toList());
+        return ResponseEntity.ok(feedbackDtos);
+    }
+}
