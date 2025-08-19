956247c0b3dadc8e0fa18611d6507c13684f3211
jjoanne21 <jjoanne2020@gmail.com>
2025-08-19 16:22:29 +0530

    filter changed 3

---
+++ src/main/java/org/godigit/ClientFeedbackAnalysisSystem/service/FilterService.java
@@ -0,0 +1,33 @@
+package org.godigit.ClientFeedbackAnalysisSystem.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.godigit.ClientFeedbackAnalysisSystem.models.Feedback;
+import org.godigit.ClientFeedbackAnalysisSystem.repository.FeedbackRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FilterService {
+    
+    private final FeedbackRepository feedbackRepository;
+
+    public FilterService(FeedbackRepository feedbackRepository) {
+        this.feedbackRepository = feedbackRepository;
+    }
+
+    public List<Feedback> filterByDate(LocalDate date) {
+        return feedbackRepository.findAll().stream()
+        .filter(f -> f.getSubmittedAt().toLocalDate().equals(date))
+        .toList();
+    }
+       public List<Feedback> getBySentiment(String sentiment)
+    {
+        return feedbackRepository.findAll().stream()
+                .filter(f -> f.getSentiment() != null &&
+                             f.getSentiment().equalsIgnoreCase(sentiment))
+                .collect(Collectors.toList());
+    }
+
+}
